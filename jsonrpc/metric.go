cedb02ad5df57ee6a682ca60ea3c7c8888e4bc10
wang liang <wangliang.f@gmail.com>
2020-04-13 13:38:52 +0800

    Update metric.go

--- jsonrpc/metric.go
+++ jsonrpc/metric.go
@@ -5,8 +5,8 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/ipfs-force-community/gosf/metric"
-	"github.com/ipfs-force-community/gosf/proc"
+	"github.com/bdchain/gosf/metric"
+	"github.com/bdchain/gosf/proc"
 )
 
 func init() {
